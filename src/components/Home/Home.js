6f91971a50086e40d3ed2232b94bae7e6d675779
0alif <alifhossain003@gmail.com>
2021-10-18 00:52:01 +0600

    init firebase

---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Footer/Footer';
+import Navigation from '../Navigation/Navigation';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <Navigation></Navigation>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div className="App">
+            404 Page not found
+        </div>
+    );
+};
+
+export default NotFound;
